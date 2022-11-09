1720387ef160bce212ab77f57b3cdb6c2be02512
UTohru <h.rc211v11@gmail.com>
2022-11-09 14:47:06 +0900

    add zsh plug

---
+++ zsh/pluginconf/exa_l.zsh
@@ -0,0 +1,4 @@
+alias ls="exa --icons"
+alias l="exa"
+alias ll="exa -l --icons -snew"
+alias la="exa -a --icons"
--- zsh/rc/plugin.zsh
+++ zsh/rc/plugin.zsh
@@ -28,19 +28,24 @@ zinit light-mode for \
 
 # == sample ==
 # zinit wait'0a' lucid \
-# 	atinit"source $ZDOTDIR/pluginconfig/script_init.zsh"
-# 	atload"source $ZDOTDIR/pluginconfig/script_loaded.zsh"
+# 	atinit"source $ZDOTDIR/pluginconfig/script_i.zsh"
+# 	atload"source $ZDOTDIR/pluginconfig/script_l.zsh"
 # 	light-mode for @hoge/plugin
 
 zinit wait'0b' lucid \
+	atload"source $ZDOTDIR/pluginconf/zsh-autocomplete_l.zsh" \
 	light-mode for @zsh-users/zsh-autosuggestions
 
 bindkey "^Y" autosuggest-accept
 
-zinit wait'0a' lucid \
-	light-mode for @marlonrichert/zsh-autocomplete
+# zinit wait'0a' lucid \
+# 	light-mode for @marlonrichert/zsh-autocomplete
+# zstyle ':autocomplete:*' default-context ''
+# zstyle ':autocomplete:*' min-delay 0.05
+# zstyle ':autocomplete:*' min-input 1
+# zstyle ':autocomplete:*' ignored-input ''
 
-zstyle ':autocomplete:*' default-context ''
-zstyle ':autocomplete:*' min-delay 0.05
-zstyle ':autocomplete:*' min-input 1
-zstyle ':autocomplete:*' ignored-input ''
+zinit wait'1' lucid \
+	from"gh-r" as"program" pick"bin/exa" \
+	atload"source $ZDOTDIR/pluginconf/exa_l.zsh" \
+	light-mode for @ogham/exa
