db3c4aad793bfe533c8c4b2810951942ab642586
UTohru <h.rc211v11@gmail.com>
2023-03-28 09:50:30 +0900

    sway env update

--- script/setup.sh
+++ script/setup.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 set -e
 
+# ===============
 #
 # Mainly create symbolic link
 #
+# ===============
 
 cdir=$(cd $(dirname $0);cd ..;pwd)
 
-#
+# ===============
+# required check
+# ===============
+package_list=("curl" "unzip" "bc")
+for p in "${package_list[@]}"
+do
+	if ! builtin command -V $p > /dev/null 2>&1; then
+		echo "[ ${package_list[@]} ] these packages are required"
+		exit 127
+	fi
+done
+
+
+# ===============
 # ignore localconf
-# 
-ignore_list=("${cdir}/zsh/localconf/rc.zsh" "${cdir}/zsh/localconf/profile.zsh" "${cdir}/i3/enable/local.conf")
+# ===============
+ignore_list=("${cdir}/zsh/localconf/rc.zsh" "${cdir}/zsh/localconf/profile.zsh" "${cdir}/i3/enable/local.conf" "${cdir}/sway/enable/local.conf")
 git update-index --skip-worktree ${ignore_list[@]}
 
-#
+
+# ===============
 # vim
-#
+# ===============
 vim_version=$(vim --version | head -n 1 | grep -o -E "[0-9]+\.[0-9]")
 vim_version_check=$(echo "scale=2;$vim_version >= 8.2" | bc)
 if [ $vim_version_check -eq 1 ]; then
@@ -38,6 +54,10 @@ else
 fi
 
 
+# ===============
+# other links
+# ===============
+
 if [ -d ~/.mlterm ]; then
 	rm -rf ~/.mlterm
 fi
@@ -45,44 +65,6 @@ ln -sf ${cdir}/mlterm ~/.mlterm
 
 ln -sf ${cdir}/.tmux.conf ~/.tmux.conf
 ln -sf ${cdir}/.latexmkrc ~/.latexmkrc
-
-if [ ! -d ~/.config ]; then
-	mkdir ~/.config
-fi
-
-if [ ! -d ${cdir}/i3/wallpaper ]; then
-	mkdir ${cdir}/i3/wallpaper
-	if [ ! -d ${cdir}/sway/wallpaper ]; then
-		ln -s ${cdir}/i3/wallpaper ~/.config/sway/wallpaper
-	fi
-fi
-
-#
-# x or wayland
-# 
-
-if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
-	if [ -d ~/.config/sway ]; then
-		rm -rf ~/.config/sway
-	fi
-	ln -sf ${cdir}/sway ~/.config/sway
-	ln -sf ${cdir}/i3/available/sway_input.conf ~/.config/sway/enable/sway_input.conf
-
-	if [ ! -d ~/.config/environment.d ]; then
-		mkdir ~/.config/environment.d
-	fi
-	# ln -sf ${cdir}/.xprofile ~/.pam_environment
-	
-	ln -sf ${cdir}/.xprofile ~/.config/environment.d/wayland.conf
-else
-	if [ -d ~/.config/i3 ]; then
-		rm -rf ~/.config/i3
-	fi
-	ln -sf ${cdir}/i3 ~/.config/i3
-	ln -sf ${cdir}/compton.conf ~/.config/compton.conf
-	ln -sf ${cdir}/.xprofile ~/.xprofile
-fi
-
 
 ln -sf ${cdir}/.zshenv ~/.zshenv
 if [ -d ~/.config/zsh ]; then
@@ -101,7 +83,75 @@ if [ -d ~/.config/efm-langserver ]; then
 fi
 ln -sf ${cdir}/efm-langserver ~/.config/efm-langserver
 
-# === wsl (ubuntu) ===
+
+if [ ! -d ~/.config ]; then
+	mkdir ~/.config
+fi
+
+if [ ! -d ${cdir}/i3/wallpaper ]; then
+	mkdir ${cdir}/i3/wallpaper
+	if [ ! -d ${cdir}/sway/wallpaper ]; then
+		ln -s ${cdir}/i3/wallpaper ~/.config/sway/wallpaper
+	fi
+fi
+
+# ===============
+# x or wayland
+# ===============
+
+if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
+	if [ -d ~/.config/sway ]; then
+		rm -rf ~/.config/sway
+	fi
+	ln -sf ${cdir}/sway ~/.config/sway
+	ln -sf ${cdir}/i3/available/sway_input.conf ~/.config/sway/enable/sway_input.conf
+
+	if [ ! -d ~/.config/environment.d ]; then
+		mkdir ~/.config/environment.d
+	fi
+	
+	if [ -f ~/.config/environment.d/wayland.conf ]; then
+		rm ~/.config/environment.d/wayland.conf
+	fi
+	ln -s ${cdir}/others/systemd_env_sway.conf ~/.config/environment.d/wayland.conf
+
+	# exportを削除してコピー
+	# oldifs=$IFS
+	# IFS=''
+	# while read line; do
+	# 	if [ "${line% *}" = "export" ]; then
+	# 		echo ${line#* } >> ~/.config/environment.d/wayland.conf
+	# 	fi
+	# done < ${cdir}/.xprofile
+	# IFS=$oldifs
+	
+	# sudo cp ${cdir}/others/Wsession /etc/lightdm/
+else
+	if [ -d ~/.config/i3 ]; then
+		rm -rf ~/.config/i3
+	fi
+	ln -sf ${cdir}/i3 ~/.config/i3
+	ln -sf ${cdir}/compton.conf ~/.config/compton.conf
+	ln -sf ${cdir}/.xprofile ~/.xprofile
+fi
+
+
+# ===============
+# install Cica font
+# ===============
+if [ ! -d ~/.local/share/fonts/Cica ]; then
+	mkdir -p ~/.local/share/fonts/Cica
+
+	cd /tmp
+	curl https://api.github.com/repos/miiton/Cica/releases/latest | jq '.assets[0].browser_download_url' | xargs curl -L -o /tmp/Cica.zip
+	unzip -o Cica.zip -d ~/.local/share/fonts/Cica
+	fc-cache -fv
+fi
+
+
+# ===============
+# wsl (ubuntu)
+# ===============
 if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
 	if command -V powershell.exe > /dev/null 2>&1; then
 		WIN_USER=$(powershell.exe '$env:USERNAME' | sed -e 's/[\r\n]\+//g')
--- script/util/shutdowntimer.sh
+++ script/util/shutdowntimer.sh
@@ -5,20 +5,18 @@
 # スケジュールの確認：
 #  date --date @$(head -1 /run/systemd/shutdown/scheduled |cut -c6-15)
 
-if [ $# != 2 ] && [ $# != 0 ]; then
+if [ $# != 0 ]; then
 	echo Error: $*
 	exit 1
 fi
 
-if [ $# -eq 0 ]; then
-	echo -n "DATE (ex. YY-MM-DD) >> "
-	read day
-	echo -n "TIME (ex. hh:mm:ss) >> "
-	read time
-else
-	day=$1
-	time=$2
-fi
+echo -n "input DATE (ex. YY-MM-DD) >> "
+read day
+echo -n "input TIME (ex. hh:mm:ss) >> "
+read time
+
+day=$1
+time=$2
 
 sudo shutdown -h +$(expr \( `date -d"${day} ${time}" +%s` - `date -d"now" +%s` \) / 60)
 #echo $(expr \( `date -d"${day} ${time}" +%s` - `date -d"now" +%s` \) / 60)
