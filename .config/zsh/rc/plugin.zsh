a5275e95788d0a85ce28838ed1682d73dea691e7
UTohru <h.rc211v11@gmail.com>
2023-11-12 19:30:21 +0900

    zsh: unload sheldon

--- .config/zsh/rc/plugin.zsh
+++ .config/zsh/rc/plugin.zsh
@@ -1,10 +1,7 @@
 
-if [ -x "$(command -v cargo)" ]; then
-	if [ ! -x "$(command -v sheldon)" ]; then
-		cargo install sheldon
-	fi
-	eval "$(sheldon source)"
-fi
+# if [ -x "$(command -v sheldon)" ]; then
+# 	eval "$(sheldon source)"
+# fi
 
 if [ -x "$(command -v zoxide)" ]; then
 	eval "$(zoxide init zsh)"
