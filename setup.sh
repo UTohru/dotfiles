a84f67c99a615aac5b5f30ada5f063884d847259
UTohru <h.rc211v11@gmail.com>
2024-07-14 15:30:42 +0900

    del: Externalizing binary installations

--- setup.sh
+++ setup.sh
@@ -8,23 +8,6 @@ set -e
 cdir="$(realpath "$(dirname "$0")")"
 
 # ===============
-# required check in this
-# ===============
-package_list=("curl" "unzip" "bc" "jq")
-not_exist_package=()
-for p in "${package_list[@]}"
-do
-	if ! builtin command -V $p > /dev/null 2>&1; then
-		not_exist_package+=($p)
-	fi
-done
-
-if [ ${#not_exist_package[@]} != 0 ]; then
-	echo "[ ${not_exist_package[@]} ] these packages are required"
-	exit 127
-fi
-
-# ===============
 # ignore localconf
 # ===============
 ignore_list=("${cdir}/.config/zsh/localconf/rc.zsh" "${cdir}/.config/zsh/localconf/profile.zsh" "${cdir}/.config/i3/enable/local.conf" "${cdir}/.config/sway/enable/local.conf")
@@ -40,26 +23,6 @@ if [ -x "$(command -v vim)" ]; then
 	fi
 	ln -sf ${cdir}/vim ~/.vim
 	ln -sf ${cdir}/.vimrc ~/.vimrc
-fi
-
-# deno
-if [ ! -x "$(command -v deno)" ]; then
-	deno_confirm="Do you install deno? (needed for vim-plugins) [Y/n]:"
-	while true; do
-		echo -n "${deno_confirm}"
-		read Ans
-		case $Ans in
-			[Yy]*)
-				curl -fsSL https://deno.land/install.sh | sh
-				break
-				;;
-			[Nn]*)
-				break
-				;;
-			*)
-				;;
-		esac
-	done
 fi
 
 
@@ -124,54 +87,5 @@ if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
 	ln -sf /mnt/c/Windows/System32/cmd.exe $HOME/.local/bin/cmd.exe
 	ln -sf /mnt/c/Windows/explorer.exe $HOME/.local/bin/explorer.exe
 fi
-
-# ===============
-# install font
-# ===============
-font_confirm="Do you install font? [Y/n]:"
-while true; do
-	echo -n "${font_confirm}"
-	read Ans
-	case $Ans in
-		[Yy]*)
-			installed=""
-			 if [ ! -d ~/.local/share/fonts/Cica ]; then
-			 	cd /tmp
-			 	mkdir -p ~/.local/share/fonts/Cica
-
-			 	curl https://api.github.com/repos/miiton/Cica/releases/latest | jq '.assets[0].browser_download_url' | xargs curl -L -o /tmp/Cica.zip
-			 	unzip -o Cica.zip -d ~/.local/share/fonts/Cica
-			 	installed="${installed}Cica"
-			fi
-			if [ ! -d ~/.local/share/fonts/Firge ]; then
-				cd /tmp
-				mkdir -p ~/.local/share/fonts/Firge
-			 
-				curl https://api.github.com/repos/yuru7/Firge/releases/latest | jq '.assets[0].browser_download_url' | xargs curl -L -o /tmp/Firge.zip
-				unzip -j -o Firge.zip -d ~/.local/share/fonts/Firge
-			 	installed="${installed}Firge"
-			fi
-			if [ ! -d ~/.local/share/fonts/udev-gothic ]; then
-				cd /tmp
-				mkdir -p ~/.local/share/fonts/udev-gothic
-
-				curl https://api.github.com/repos/yuru7/udev-gothic/releases/latest | jq '.assets[0].browser_download_url' | xargs curl -L -o /tmp/udev-gothic.zip
-				unzip -j -o udev-gothic.zip -d ~/.local/share/fonts/udev-gothic
-			 	installed="${installed}udev-gothic"
-			fi
-
-			# recache
-			if [ -n "${installed}" ]; then
-				fc-cache -fv
-			fi
-			break
-			;;
-		[Nn]*)
-			break
-			;;
-		*)
-			;;
-	esac
-done
 
 echo "complete!"
